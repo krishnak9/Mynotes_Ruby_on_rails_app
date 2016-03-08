c0be44b373126603db8ca22ada0728e0fcc3ed4f
Krishna <krishnaklooper@gmail.com>
2016-03-08 22:31:46 +0530

    now root is based on loggedin/guest

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
+  resources:notes
+  authenticated :user do
+    root 'notes#index', as: "authenticated_root"
+  end 
   root 'home#index'
   resources:notes
 
