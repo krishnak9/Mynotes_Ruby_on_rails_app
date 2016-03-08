e426a1f161ad32e9ee158a972fb229ebf295b0e5
Krishna <krishnaklooper@gmail.com>
2016-03-08 22:18:46 +0530

    added association b/w user and notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,12 +6,12 @@ class NotesController < ApplicationController
    def show
    end
    def new
-   	@note = Note.new
+   	@note = current_user.notes.build
    end
    def edit
    end
    def create
-   	@note = Note.new(note_params)
+   	@note = current_user.notes.build(note_params)
 
    	if @note.save
    		redirect_to @note
